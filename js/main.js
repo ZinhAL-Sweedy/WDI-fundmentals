dc6b66e85e9b8695deab4b5b21b3362c8d10388e
Zz <{zin_tafe}@yahoo.com.au>
2017-05-08 21:11:04 +1000

    created game logic 3

--- js/main.js
+++ js/main.js
@@ -5,22 +5,51 @@
  var cardFour = "king";*/
 /* console.log("User flipped "+ cardOne);
  console.log("User flipped " + cardFour);*/
+
+
+
+
 var cards=["queen", "queen", "king", "king"];
 var cardsInPlay=[];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped "+cardsInPlay);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped "+cardsInPlay);
-if ((cardsInPlay.length) ===2)
-{
-	if (cardsInPlay[0]===cardsInPlay[1])
-	{
-		 alert("You found a match!");
-	}
-	else
-	{
-		 alert("Sorry, try again.");
-	}
+
+var checkForMatch=function(){
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+      alert("You found a match!");
+  } 
+  else {
+      console.log("Sorry, try again.");
+     alert("Sorry, try again.");
+  }
 }
+
+
+var flipCard=function(cardId){
+
+	console.log("User flipped over "+ cards[cardId]);
+	// var cardOne = cards[0];
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+
+	//var cardTwo = cards[2];
+	//cardsInPlay.push(cardTwo);
+	
+};
+
+
+flipCard(0);
+flipCard(2);
+
+	/*console.log("User flipped "+cardsInPlay);
+	if ((cardsInPlay.length) ===2)
+	{
+		if (cardsInPlay[0]===cardsInPlay[1])
+		{
+			 alert("You found a match!");
+		}
+		else
+		{
+			 alert("Sorry, try again.");
+		}
+}*/
